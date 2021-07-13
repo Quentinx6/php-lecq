16ba654fc67b2ea32ff9b5e72113f7bcd55fc049
Quentinx6 <quentin.lecompte@3wa.io>
2021-07-13 09:45:03 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Premier back</title>
+</head>
+<body>
+    <div>
+        <h1>PHP procédural</h1>
+        <hr>
+        <p>J'apprends et je me perfectionne.</p>
+    </div>
+    <button>Home</button>
+    <button>Ajouter des données</button>
+    <p>© 2021 Quentin Lecompte - PHP</p>
+</body>
+</html>
